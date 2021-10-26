f7e7b05a4582ad7871d985188af2a6786573b2ec
tangym27 <mt4119@nyu.edu>
2021-10-26 17:58:43 -0400

    added accounts to homepage

--- front-end/src/pages/Homepage.js
+++ front-end/src/pages/Homepage.js
@@ -1,9 +1,26 @@
-import React from 'react'; 
+import React from "react";
+import Accounts from "./Accounts.js";
+import Button from "@mui/material/Button";
+import { Link } from "react-router-dom";
+import "../components/css/Homepage.css";
 
 const Homepage = () => {
-    return (
-        <h1>This is homepage</h1>
-    );
-}
+  return (
+    <div>
+      <br />
+      <div class="container-home">
+        <Accounts> </Accounts>
+        <Button
+          variant="contained"
+          id="new-account-btn"
+          component={Link}
+          to="/accounts"
+        >
+          See More
+        </Button>
+      </div>
+    </div>
+  );
+};
 
 export default Homepage;
