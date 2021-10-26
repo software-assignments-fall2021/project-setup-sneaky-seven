ea862f4143bf61266d6da2f3b6b13fb656644a77
tangym27 <mt4119@nyu.edu>
2021-10-26 04:02:55 -0400

    just added css thematic elements

--- front-end/src/components/ContactForm.js
+++ front-end/src/components/ContactForm.js
@@ -1,31 +1,58 @@
 import React, { useState } from "react";
+import Button from "@mui/material/Button";
+import { Link } from "react-router-dom";
+import "./css/ContactForm.css";
 
-// TODO: connect to backend 
+// TODO: connect to backend
 const ContactForm = () => {
-  const [status, setStatus] = useState("Submit")
+  const [status, setStatus] = useState("Submit");
 
   const handleSubmit = async (e) => {
-    setStatus("Sending")
-  }
+    setStatus("Sending");
+  };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <div><h1>Contact Us</h1></div>
-      <div>
-        <label htmlFor="name"><b>Name:</b></label>
-        <input type="text" id="name" required />
-      </div>
-      <div>
-        <label htmlFor="email"><b>Email:</b></label>
-        <input type="email" id="email" required />
-      </div>
-      <div>
-        <label htmlFor="message"><b>Message:</b></label>
-        <textarea id="message" required />
-      </div>
-      <button type="submit">{status}</button>
-    </form>
-  )
-}
+    <div>
+      <br />
+      <form onSubmit={handleSubmit} class="container">
+        <div>
+          <h1>Contact Us</h1>
+        </div>
+        <hr /> <br />
+        <div class="form">
+          <div>
+            <label htmlFor="name">
+              <b>Name:</b>
+            </label>
+            <br />
 
-export default ContactForm
+            <input type="text" id="name" required />
+          </div>
+          <br />
+          <div>
+            <label htmlFor="email">
+              <b>Email:</b>
+            </label>
+            <br />
+
+            <input type="email" id="email" required />
+          </div>
+          <br />
+          <div>
+            <label htmlFor="message">
+              <b>Message:</b>
+            </label>
+            <br />
+            <textarea id="message" required />
+          </div>
+        </div>
+        <br />
+        <Button type="submit" component={Link} to="/" variant="contained">
+          {status}
+        </Button>
+      </form>
+    </div>
+  );
+};
+
+export default ContactForm;
