1c50aa5fd59537dc8188d943ac9fb067a9105799
tangym27 <mt4119@nyu.edu>
2021-10-26 02:45:22 -0400

    login and registration

--- front-end/src/components/Navbar/NavbarData.js
+++ front-end/src/components/Navbar/NavbarData.js
@@ -1,44 +1,50 @@
-import React from 'react';
-import * as FaIcons from 'react-icons/fa';
-import * as AiIcons from 'react-icons/ai';
-import * as IoIcons from 'react-icons/io';
+import React from "react";
+import * as FaIcons from "react-icons/fa";
+import * as AiIcons from "react-icons/ai";
+import * as IoIcons from "react-icons/io";
 
 // TODO: update icons
 export const NavbarData = [
   {
-    title: 'Home',
-    path: '/',
+    title: "Home",
+    path: "/homepage",
     icon: <AiIcons.AiFillHome />,
-    cName: 'nav-text'
+    cName: "nav-text",
   },
   {
-    title: 'Accounts',
-    path: '/accounts',
+    title: "Accounts",
+    path: "/accounts",
     icon: <IoIcons.IoIosPaper />,
-    cName: 'nav-text'
+    cName: "nav-text",
   },
   {
-    title: 'Transactions',
-    path: '/transactions',
+    title: "Transactions",
+    path: "/transactions",
     icon: <FaIcons.FaCartPlus />,
-    cName: 'nav-text'
+    cName: "nav-text",
   },
   {
-    title: 'Statistics',
-    path: '/statistics',
+    title: "Statistics",
+    path: "/statistics",
     icon: <IoIcons.IoMdPeople />,
-    cName: 'nav-text'
+    cName: "nav-text",
   },
   {
-    title: 'FAQs',
-    path: '/FAQs',
+    title: "FAQs",
+    path: "/FAQs",
     icon: <FaIcons.FaEnvelopeOpenText />,
-    cName: 'nav-text'
+    cName: "nav-text",
   },
   {
-    title: 'Contact Us',
-    path: '/contact',
+    title: "Contact Us",
+    path: "/contact",
     icon: <IoIcons.IoMdHelpCircle />,
-    cName: 'nav-text'
-  }
+    cName: "nav-text",
+  },
+  {
+    title: "Log Out",
+    path: "/",
+    icon: <FaIcons.FaDoorOpen />,
+    cName: "nav-text",
+  },
 ];
--- front-end/src/components/Navbar/NavbarWrapper.js
+++ front-end/src/components/Navbar/NavbarWrapper.js
@@ -1,29 +1,33 @@
-import React, { PureComponent } from 'react'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Navbar from './Navbar';
-import Homepage from '../../pages/Homepage'
-import Accounts from '../../pages/Accounts'
-import Transactions from '../../pages/Transactions'
-import Statistics from '../../pages/Statistics'
-import Contact from '../../pages/Contact'
-import FAQs from '../../pages/FAQs'
+import React, { PureComponent } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Navbar from "./Navbar";
+import Login from "../../pages/Login";
+import Registration from "../../pages/Registration";
+import Homepage from "../../pages/Homepage";
+import Accounts from "../../pages/Accounts";
+import Transactions from "../../pages/Transactions";
+import Statistics from "../../pages/Statistics";
+import Contact from "../../pages/Contact";
+import FAQs from "../../pages/FAQs";
 
 export class NavbarWrapper extends PureComponent {
-    render() {
-        return (
-            <Router>
-                <Navbar />
-                <Switch>
-                    <Route path='/' exact component={Homepage} />
-                    <Route path='/accounts' exact component={Accounts} />
-                    <Route path='/transactions' exact component={Transactions} />
-                    <Route path='/statistics' exact component={Statistics} />
-                    <Route path='/FAQs' exact component={FAQs} />
-                    <Route path='/contact' exact component={Contact} />
-                </Switch>
-            </Router>
-        )
-    }
+  render() {
+    return (
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path="/" exact component={Login} />
+          <Route path="/registration" exact component={Registration} />
+          <Route path="/homepage" exact component={Homepage} />
+          <Route path="/accounts" exact component={Accounts} />
+          <Route path="/transactions" exact component={Transactions} />
+          <Route path="/statistics" exact component={Statistics} />
+          <Route path="/FAQs" exact component={FAQs} />
+          <Route path="/contact" exact component={Contact} />
+        </Switch>
+      </Router>
+    );
+  }
 }
 
-export default NavbarWrapper
+export default NavbarWrapper;
