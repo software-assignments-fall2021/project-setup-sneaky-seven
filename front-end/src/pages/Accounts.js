08f80726e43f6ba178afd666af43b9d0657436be
tangym27 <mt4119@nyu.edu>
2021-10-26 16:09:59 -0400

    rebase into master

--- front-end/src/pages/Accounts.js
+++ front-end/src/pages/Accounts.js
@@ -1,49 +1,54 @@
-import './css/Accounts.css'
-import React, { useEffect, useState } from 'react';
-import AccountDetail from '../components/AccountDetail/AccountDetail';
-import AccountsPage from '../components/Accounts/AccountsPage';
-import { backupData } from '../components/Accounts/AccountBackupData';
-import axios from 'axios';
-
+import "./css/Accounts.css";
+import React, { useEffect, useState } from "react";
+import AccountDetail from "../components/AccountDetail/AccountDetail";
+import AccountsPage from "../components/Accounts/AccountsPage";
+import { backupData } from "../components/Accounts/AccountBackupData";
+import axios from "axios";
 
 // To-do: identify a better color scheme
-// To-do 2: use the bankId in props when saving to DB 
+// To-do 2: use the bankId in props when saving to DB
 /**
  * This function handles the rendering of the list of accounts
  * When the user clicks on an account, it will render the details for that
  * particular account which is in the AccountDetail panel
- * 
+ *
  * @returns Account page component
  */
 function Accounts() {
+  const [data, setData] = useState([]);
+  const [showDetail, setShowDetail] = useState(false);
+  const [bankDetailName, setBankDetailName] = useState("");
 
-    const [data, setData] = useState([]);
-    const [showDetail, setShowDetail] = useState(false);
-    const [bankDetailName, setBankDetailName] = useState("")
+  useEffect(() => {
+    console.log("fetching accounts data for user");
+    axios("https://my.api.mockaroo.com/budget_app_accounts.json?key=9a5445a0")
+      .then((response) => {
+        setData(response.data);
+      })
+      .catch((err) => {
+        console.log(
+          "Something went wrong. We're probably out of requests for the day!"
+        );
+        console.error(err);
+        setData(backupData);
+      });
+  });
 
-    useEffect(() => {
-        console.log("fetching accounts data for user");
-        axios('https://my.api.mockaroo.com/budget_app_accounts.json?key=9a5445a0')
-            .then((response) => {
-                setData(response.data)
-            })
-            .catch((err) => {
-                console.log("Something went wrong. We're probably out of requests for the day!")
-                console.error(err);
-                setData(backupData)
-            })
-    })
+  const renderPage = showDetail ? (
+    <AccountDetail
+      showAccountDetail={setShowDetail}
+      setBankDetailHeader={setBankDetailName}
+      bankName={bankDetailName}
+    />
+  ) : (
+    <AccountsPage
+      banks={data}
+      setShowDetail={setShowDetail}
+      setBankDetailName={setBankDetailName}
+    />
+  );
 
-    const renderPage = showDetail ? 
-    <AccountDetail 
-        showAccountDetail={setShowDetail}
-        setBankDetailHeader={setBankDetailName} 
-        bankName={bankDetailName}
-        /> : <AccountsPage banks={data} setShowDetail={setShowDetail} setBankDetailName={setBankDetailName} />
-
-    return <div className="container">
-                {renderPage}
-            </div>
+  return <div className="container-account">{renderPage}</div>;
 }
 
 export default Accounts;
