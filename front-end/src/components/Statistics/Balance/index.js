ad5e23925c48cdffa254c5a3eecf335046366057
tangym27 <mt4119@nyu.edu>
2021-10-26 22:29:48 -0400

    added homepage

--- front-end/src/components/Statistics/Balance/index.js
+++ front-end/src/components/Statistics/Balance/index.js
@@ -1,28 +1,30 @@
-import PieChart from '../Charts/PieChart'
-import BarChart from '../Charts/BarChart'
+import PieChart from "../Charts/PieChart";
+import BarChart from "../Charts/BarChart";
+import "../../css/Statistics.css";
 
-const axios = require('axios').default
+const axios = require("axios").default;
 
-const data = axios.get('https://my.api.mockaroo.com/budget_web_app.json?key=d9fa63b0')
-    .then(response => {
-        console.log('test')
-    })
+const data = axios
+  .get("https://my.api.mockaroo.com/budget_web_app.json?key=d9fa63b0")
+  .then((response) => {
+    console.log("test");
+  });
 
 const Balance = () => {
-    return (
-        <div>
-            <h1>Balance</h1>
-            <BarChart
-                name="Balance Trend"
-                data={[
-                    ['Category', 'Balance'], // mock data
-                    ['10/25/2021', 4],
-                    ['10/26/2021', 8],
-                    ['10/27/2021', 14]
-                ]}
-            />
-        </div>
-    )
+  return (
+    <div class="container-stats">
+      <h1>Balance</h1>
+      <BarChart
+        name="Balance Trend"
+        data={[
+          ["Category", "Balance"], // mock data
+          ["10/25/2021", 4],
+          ["10/26/2021", 8],
+          ["10/27/2021", 14],
+        ]}
+      />
+    </div>
+  );
 };
 
 export default Balance;
--- front-end/src/pages/Homepage.js
+++ front-end/src/pages/Homepage.js
@@ -1,5 +1,8 @@
 import React from "react";
 import Accounts from "./Accounts.js";
+import Transactions from "./Transactions.js";
+import Spending from "../components/Statistics/Spending";
+import Balance from "../components/Statistics/Balance";
 import Button from "@mui/material/Button";
 import { Link } from "react-router-dom";
 import "../components/css/Homepage.css";
@@ -15,6 +18,43 @@ const Homepage = () => {
           id="new-account-btn"
           component={Link}
           to="/accounts"
+        >
+          See More
+        </Button>
+      </div>
+      <hr />
+      <div class="container-home">
+        <Transactions> </Transactions>
+        <Button
+          variant="contained"
+          id="new-account-btn"
+          component={Link}
+          to="/transactions"
+        >
+          See More
+        </Button>
+      </div>
+      <hr />
+      <div class="statContainer">
+        <Spending> </Spending>
+        <Button
+          variant="contained"
+          id="new-account-btn"
+          component={Link}
+          to="/statistics"
+        >
+          {" "}
+          See More
+        </Button>
+      </div>
+      <hr />
+      <div class="statContainer">
+        <Balance> </Balance>
+        <Button
+          variant="contained"
+          id="new-account-btn"
+          component={Link}
+          to="/statistics"
         >
           See More
         </Button>
