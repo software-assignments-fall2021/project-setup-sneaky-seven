7cfe98cb8b7e73c68f166d8b79c581b1c95fdab5
tangym27 <mt4119@nyu.edu>
2021-11-05 23:41:37 -0400

    filtered categories and added unit test

---
+++ back-end/test/test.js
@@ -0,0 +1,99 @@
+const chai = require("chai");
+const chaiHTTP = require("chai-http");
+chai.use(chaiHTTP);
+const expect = chai.expect;
+const path = require("path");
+require("mocha-sinon");
+Object.assign(global, require(path.join(__dirname, "../app.js")));
+
+describe("testing routes", () => {
+  describe("testing get route /api/categories", () => {
+    it("returns a 200 status", function (cb) {
+      chai
+        .request(app)
+        .get("/api/categories")
+        .end(function (err, res) {
+          expect(res).to.have.status(200);
+          cb();
+        });
+    });
+    it("contains pre-defined plaid categories", function (cb) {
+      chai
+        .request(app)
+        .get("/api/categories")
+        .end(function (err, res) {
+          expect(res.body).to.include.members(["Bank Fees"]);
+          cb();
+        });
+    });
+  });
+  describe("testing post route /api/create_link_token", () => {
+    it("returns a 200 status", function (cb) {
+      chai
+        .request(app)
+        .post("/api/create_link_token")
+        .end(function (err, res) {
+          expect(res).to.have.status(200);
+          cb();
+        });
+    });
+  });
+  describe("testing post route /api/set_access_token", () => {
+    it("returns a 200 status", function (cb) {
+      chai
+        .request(app)
+        .post("/api/set_access_token")
+        .end(function (err, res) {
+          expect(res).to.have.status(200);
+          cb();
+        });
+    });
+  });
+});
+
+describe("testing printing/debugging functions", () => {
+  beforeEach(function () {
+    this.sinon.stub(console, "log");
+  });
+
+  describe("testing prettyPrintResponse function", () => {
+    it("reads api response and prints out relevant data information", () => {
+      const fakeData = { data: "important data", status: 200, language: "en" };
+      prettyPrintResponse(fakeData);
+      expect(console.log.calledOnce).to.be.true;
+      expect(console.log.calledWith("important data")).to.be.true;
+    });
+  });
+
+  describe("testing formatError function", () => {
+    it("reads api response and prints out relevant error information", () => {
+      const fakeData = { data: "important data", status: 500, language: "en" };
+      expect(formatError(fakeData)).to.have.property("error");
+      expect(formatError(fakeData).error).to.have.property("status_code");
+    });
+  });
+});
+
+describe("testing filter categories function", () => {
+  it("returns categories without duplicates", () => {
+    const categories = [
+      { hierarchy: ["cat0"] },
+      { hierarchy: ["cat1"] },
+      { hierarchy: ["cat0"] },
+      { hierarchy: ["cat2"] },
+      { hierarchy: ["cat3"] },
+      { hierarchy: ["cat0"] },
+      { hierarchy: ["cat0"] },
+    ];
+    expect(filterCategories(categories)).to.include.members([
+      "cat0",
+      "cat1",
+      "cat2",
+      "cat3",
+    ]);
+  });
+  it("returns an empty list when user has no categories", () => {
+    const categories = [];
+    expect(filterCategories(categories)).to.include.members([]);
+  });
+});
