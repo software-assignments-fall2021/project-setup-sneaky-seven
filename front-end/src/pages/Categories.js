6f25f2466adc3478b877afcbe5a793fd0e3141e9
Michelle Tang <mt4119@nyu.edu>
2021-11-07 01:26:08 -0500

    removed a bit of MUI that interacted poorly with react-router-dom

--- front-end/src/pages/Categories.js
+++ front-end/src/pages/Categories.js
@@ -1,27 +1,24 @@
 import React from 'react'
-import Button from '@mui/material/Button';
 import AddIcon from '@mui/icons-material/Add';
 import api from '../api';
 import './css/Categories.css';
 import { useHistory } from 'react-router-dom';
-import { iconNameToComponent, useAsync, globalStyles } from '../utils';
+import { iconNameToComponent, useAsync, styles } from '../utils';
 
 const Categories = () => {
     const { data: categoryData } = useAsync(api.getCategoryList, []);
     const categoryList = categoryData ?? [];
     const history = useHistory();
-    const addCategory = React.useCallback(() => history.push("/categories/addCaetgory"), [history]);
+    const addCategory = React.useCallback(() => history.push("/categories/addCategory"), [history]);
 
     return (
         <div>
             <h1>Category</h1>
-            <Button
-                style={{justifyContent: "flex-start", textAlign: 'left'}}
-                variant="contained"
-                sx={{ fontWeight: 'bold', }}
-                size="large"
-                href="categories/addCategory"
-            ><AddIcon />Add Category</Button>
+            <button
+                type={'button'}
+                className={styles.muiButton}
+                onClick={addCategory}
+            ><AddIcon />Add Category</button>
             <ul>
                 {categoryList.map((item, index) => {
                     const Icon = iconNameToComponent[item.icon];
