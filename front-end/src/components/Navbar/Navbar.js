6f7d98eadbd0ea994bdae98fb3060cd9ac2d9569
Michelle Tang <mt4119@nyu.edu>
2021-11-07 18:51:54 -0500

    small refactors

--- front-end/src/components/Navbar/Navbar.js
+++ front-end/src/components/Navbar/Navbar.js
@@ -1,26 +1,77 @@
 import React, { useState } from 'react';
-import * as FaIcons from 'react-icons/fa';
-import * as AiIcons from 'react-icons/ai';
+import * as FaIcons from "react-icons/fa";
+import * as AiIcons from "react-icons/ai";
+import * as IoIcons from "react-icons/io";
+import * as BiIcons from "react-icons/bi";
 import { Link } from 'react-router-dom';
-import { NavbarData } from './NavbarData';
 import '../css/Navbar.css'
 import { IconContext } from 'react-icons';
 
+// TODO(jennifer): update icons
+export const NavbarData = [
+  {
+    title: "Home",
+    path: "/homepage",
+    icon: <AiIcons.AiFillHome />,
+    cName: "nav-text",
+  },
+  {
+    title: "Accounts",
+    path: "/accounts",
+    icon: <AiIcons.AiFillBank />,
+    cName: "nav-text",
+  },
+  {
+    title: "Transactions",
+    path: "/transactions",
+    icon: <AiIcons.AiOutlineTransaction />,
+    cName: "nav-text",
+  },
+  {
+    title: "Statistics",
+    path: "/statistics",
+    icon: <IoIcons.IoIosStats />,
+    cName: "nav-text",
+  },
+  {
+    title: "Categories",
+    path: "/categories",
+    icon: <BiIcons.BiCategory />,
+    cName: "nav-text",
+  },
+  {
+    title: "FAQs",
+    path: "/FAQs",
+    icon: <IoIcons.IoMdHelpCircle />,
+    cName: "nav-text",
+  },
+  {
+    title: "Contact Us",
+    path: "/contact",
+    icon: <FaIcons.FaEnvelopeOpenText />,
+    cName: "nav-text",
+  },
+  {
+    title: "Log Out",
+    path: "/",
+    icon: <FaIcons.FaDoorOpen />,
+    cName: "nav-text",
+  },
+];
+
 const Navbar = () => {
   const [sidebar, setSidebar] = useState(false);
-
-  const showSidebar = () => setSidebar(!sidebar);
 
   return (
     <>
       <IconContext.Provider value={{ color: '#fff' }}>
         <div className='navbar'>
           <Link to='#' className='menu-bars'>
-            <FaIcons.FaBars onClick={showSidebar} />
+            <FaIcons.FaBars onClick={() => setSidebar(true)} />
           </Link>
         </div>
         <nav className={sidebar ? 'nav-menu active' : 'nav-menu'}>
-          <ul className='nav-menu-items' onClick={showSidebar}>
+          <ul className='nav-menu-items' onClick={() => setSidebar(false)}>
             <li className='navbar-toggle'>
               <Link to='#' className='menu-bars'>
                 <AiIcons.AiOutlineClose />
--- front-end/src/pages/Statistics.js
+++ front-end/src/pages/Statistics.js
@@ -1,7 +1,24 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
+import { NavLink as Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import './css/Statistics.css';
+import Balance from "../components/Statistics/Balance";
+import Spending from "../components/Statistics/Spending";
 import api from "../api";
-import StatsNavbarWrapper from "../components/Statistics/StatsNavbar/StatsNavbarWrapper";
 import { useAsync } from "../utils";
+
+const Navbar = () => {
+  return (
+      <div className="statsNavbar">
+        <Link className="statsNavLink" to="/statistics/spending">
+          Spending
+        </Link>
+        <Link className="statsNavLink" to="/statistics/balance">
+          Balance
+        </Link>
+      </div>
+  );
+};
 
 const Statistics = () => {
   const { data: transactionData } = useAsync(api.getAllTransactions, []);
@@ -10,7 +27,21 @@ const Statistics = () => {
   return (
     <div>
       <h1>Statistics</h1>
-      <StatsNavbarWrapper data={transactions} />
+      <Navbar />
+      <Switch>
+        <Route
+          path="/statistics/spending"
+          exact
+        ><Spending data={transactions} /></Route>
+        <Route
+          path="/statistics/balance"
+          exact
+        ><Balance data={transactions} /></Route>
+        <Route
+          path="/statistics"
+          exact
+        ><Balance data={transactions} /></Route>
+      </Switch>
     </div>
   );
 };
