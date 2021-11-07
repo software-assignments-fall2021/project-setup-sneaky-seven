e36d54054100951ab7f43fe13cf177a066e16f5e
tangym27 <mt4119@nyu.edu>
2021-11-07 00:39:28 -0400

    added contact form backend

---
+++ front-end/src/components/ContactFormConfirm.js
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+import Button from "@mui/material/Button";
+import { useHistory } from "react-router-dom";
+import "./css/ContactForm.css";
+import axios from "axios";
+import { useAsync } from "../utils";
+
+const ContactFormConfirm = () => {
+  const { data } = useAsync(async () => axios.get("/contactInfo"), []);
+  const name = data?.data?.name ?? "";
+  const email = data?.data?.email ?? "";
+  console.log("body:");
+  console.log(data);
+  let msg = "";
+  if (name === "") {
+    msg = "Please submit a Contact Us request first!";
+  } else {
+    msg =
+      "Thanks " +
+      name +
+      "! We will do our best to send a response to " +
+      email +
+      " in a timely manner.";
+  }
+  return (
+    <div>
+      <br />
+      <div className="container-confirm">
+        <h1>{msg}</h1>
+      </div>
+    </div>
+  );
+};
+
+export default ContactFormConfirm;
