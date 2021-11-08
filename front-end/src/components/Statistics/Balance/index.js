4674c49f77694de90fdbd729e23a5f14d01289b5
tangym27 <mt4119@nyu.edu>
2021-11-07 23:07:41 -0500

    finished incomplete stats page

--- front-end/src/components/Statistics/Balance/index.js
+++ front-end/src/components/Statistics/Balance/index.js
@@ -1,3 +1,6 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { useAsync } from "../../../utils";
 import PieChart from "../Charts/PieChart";
 import BarChart from "../Charts/BarChart";
 import api from "../../../api/index";
@@ -31,14 +34,36 @@ const BalanceByAccountList = ({ accountToBalance }) => {
 };
 
 const Balance = ({ data }) => {
+  const [token, setToken] = useState(null);
+  const [bankData, setBankData] = useState([]);
+
+  // generate a link_token (public token) and get linked banks
+  useAsync(async () => {
+    axios.post("/api/create_link_token", {}).then((resp) => {
+      setToken(resp.data.link_token);
+    });
+
+    axios
+      .post("/api/get_bank_accounts", {
+        access_token_object: localStorage.getItem("access_token_object"),
+      })
+      .then((resp) => {
+        if (!resp.data.err) {
+          setBankData(resp.data);
+        }
+      });
+  }, []);
+
+  const base = bankData[0]?.balances?.available ?? 0;
+  console.log(base);
   // Gather data
   const accountToBalance = {};
   const dateToBalance = {};
 
   data.forEach((transaction) => {
-    const account = transaction.account;
+    const account = base + transaction.account;
     const date = transaction.date;
-    const amount = transaction.amount;
+    const amount = base + transaction.amount;
 
     if (accountToBalance[account]) {
       accountToBalance[account] += amount;
@@ -61,11 +86,13 @@ const Balance = ({ data }) => {
   }
 
   return (
-    <div>
-      <h1>Balance</h1>
-      <BarChart name="Balance Trend" data={balanceTrend} />
-      <BalanceByAccountList accountToBalance={accountToBalance} />
-    </div>
+    <>
+      <br />
+      <div className="container-s">
+        <h1>Balance</h1>
+        <BarChart name="Balance Trend" data={balanceTrend} />
+      </div>
+    </>
   );
 };
 
