ba3ac1e89e0eea246db65c383c9031c7dbd31550
Michelle Tang <mt4119@nyu.edu>
2021-11-12 05:23:06 -0500

    fixed width issue, refactored a lil bit

--- front-end/src/pages/TransactionDetail.js
+++ front-end/src/pages/TransactionDetail.js
@@ -1,48 +1,40 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { useParams } from "react-router";
 import api from "../api";
 import { useAsync } from "../utils";
-import { getTransactionById } from "../api/mocks";
 import { IoIosArrowRoundBack } from "react-icons/io";
 import { useHistory } from "react-router";
 import "./css/TransactionDetail.css";
 import { DateTime } from "luxon";
 import CategoryIcon from "../components/CategoryIcon";
 
-const currenySymbol = {
-  USD: "$",
-};
-
 const Field = ({ title, text, children }) => {
-  return (<div className="transactionField">
-    <label className="transactionFieldLabel">{title}</label>
-    {children ??  <p className="transactionFieldText">{text}</p>}
-  </div>);
+  return (
+    <div className="transactionField">
+      <label className="transactionFieldLabel">{title}</label>
+      {children ??  <p className="transactionFieldText">{text}</p>}
+    </div>
+  );
 };
 
 export function TransactionDetail() {
   let { id } = useParams();
   const { data: categoryData } = useAsync(api.getCategoryList, []);
   const categoryList = categoryData ?? [];
-  const [selectedCategory, setSelectedCategory] = useState(undefined);
+  const [selectedCategory, setSelectedCategory] = React.useState(undefined);
   const history = useHistory();
-  const [notes, setNotes] = useState("");
-  const [checkedHide, setCheckedHide] = useState(false);
-  const [checkedDuplicate, setCheckedDuplicate] = useState(false);
+  const [notes, setNotes] = React.useState("");
+  const [checkedHide, setCheckedHide] = React.useState(false);
+  const [checkedDuplicate, setCheckedDuplicate] = React.useState(false);
 
   //retreive transaction
   const { data: transaction } = useAsync(async () => {
     const result = await api.getTransactionById(id);
     setSelectedCategory(result?.category);
     return result;
-  }, [id]);
-
-  function handleBackClick() {
-    history.goBack();
-  }
+  }, [id, setSelectedCategory]);
 
   const amountColor = transaction?.amount >= 0 ? "green" : "red";
-  const symbol = currenySymbol[transaction?.currency];
 
   const formattedDate = transaction?.date
     ? DateTime.fromISO(transaction?.date).toFormat("DDDD")
@@ -51,11 +43,11 @@ export function TransactionDetail() {
   return (
     <article className="transactionView">
       <div className="subheader">
-        <div className="transBackButton" onClick={handleBackClick}>
+        <div className="transBackButton" onClick={() => history.goBack()}>
           <IoIosArrowRoundBack size={50} />
         </div>
         <h3 className="transactionAmount" style={{ color: amountColor }}>
-          {symbol} {transaction?.amount}
+          {"$"}{transaction?.amount}
         </h3>
         <div style={{ width: "50px", height: "50px" }}></div>
       </div>
