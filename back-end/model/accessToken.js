161859aa29dd75d2ec79e4f6c62bc7157e203760
Alan Chen <azc242@nyu.edu>
2021-11-18 13:42:00 -0500

    change access token collection name

--- back-end/model/accessToken.js
+++ back-end/model/accessToken.js
@@ -1,4 +1,4 @@
 const mongoose = require("mongoose");
 const accessTokenSchema = require('../schema/accessToken')
 
-module.exports = mongoose.model('accessTokenModel', accessTokenSchema);
+module.exports = mongoose.model('accessToken', accessTokenSchema);
