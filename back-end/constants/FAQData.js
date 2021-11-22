31223464b56ebe1c8d066afb3b8b819174e52c4c
tangym27 <mt4119@nyu.edu>
2021-11-21 22:05:55 -0500

    changed faq data to be actual questions

--- back-end/constants/FAQData.js
+++ back-end/constants/FAQData.js
@@ -2,20 +2,23 @@
  * Default placeholder FAQ Data. This will be in the DB in the future.
  */
 const FAQData = {
-    rows: [
-      {
-        title: "Lorem ipsum dolor sit amet 1?",
-        content: "Lorem ipsum dolor sit amet 1",
-      },
-      {
-        title: "Lorem ipsum dolor sit amet 2?",
-        content: "Lorem ipsum dolor sit amet 2",
-      },
-      {
-        title: "Lorem ipsum dolor sit amet 3?",
-        content: "Lorem ipsum dolor sit amet 3",
-      },
-    ],
+  rows: [
+    {
+      title: "Why did I need to create an account to use this?",
+      content:
+        "We store access tokens to show your bank account information. That should remain exclusive to each account. You don't want your bank account information public to everyone!",
+    },
+    {
+      title: "How do I add a category?",
+      content:
+        "Go to the category tab on the hamburger menu. Click add a category and choose your icon!",
+    },
+    {
+      title: "How do I move a transaction to a specific category?",
+      content:
+        "Click on any transaction. Hover over the icons to see which category you would like to move it do. Click!",
+    },
+  ],
 };
-  
+
 module.exports = FAQData;
