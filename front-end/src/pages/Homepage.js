85fe31238372d754921ad9f2fd8380c6618b94c9
tangym27 <mt4119@nyu.edu>
2021-11-21 23:12:58 -0500

    fixed linking

--- front-end/src/pages/Homepage.js
+++ front-end/src/pages/Homepage.js
@@ -10,98 +10,105 @@ import "../components/css/Homepage.css";
 import api from "../api";
 import axios from "axios";
 import HomePageTransactionList from "../components/HomePageTransactionList.js";
-import DateTime from "luxon"
+import DateTime from "luxon";
 
 const Homepage = () => {
   // const { data } = useAsync(async () => axios.get("/api/get_transactions"), []);
   // const transactions = data?.data ?? [];
 
-    // Used for balances
-    const [accountToBalance, setAccountToBalance] = useState({});
-    const [balanceTrend, setBalanceTrend] = useState([])
-    const [transactions, setTransactions] = useState([])
+  // Used for balances
+  const [accountToBalance, setAccountToBalance] = useState({});
+  const [balanceTrend, setBalanceTrend] = useState([]);
+  const [transactions, setTransactions] = useState([]);
 
-    // Used for spendings
-    const [spendingByCategories, setSpendingByCategories] = useState([])
-    const [spendingTrend, setSpendingTrend] = useState([])
+  // Used for spendings
+  const [spendingByCategories, setSpendingByCategories] = useState([]);
+  const [spendingTrend, setSpendingTrend] = useState([]);
 
-    useAsync(async () => {
-        // Get bank accounts to show accounts by balance
-        const bankAccounts = await api.getBankAccounts()
-        bankAccounts.forEach(account => setAccountToBalance(
-            Object.assign(
-                accountToBalance,
-                {[account.name]: account.balances.current}
-            )
-        ))
-
-        // Get the balance by trend
-        const t = await api.getAllTransactions()
-        setTransactions(t)
-
-        const dateToNet = {}
-        t.forEach(transaction => {
-            const date = transaction.date
-            const amount = transaction.amount
-            const val = dateToNet[date] ? dateToNet[date] + amount : amount
-            dateToNet[date] = val
+  useAsync(async () => {
+    // Get bank accounts to show accounts by balance
+    const bankAccounts = await api.getBankAccounts();
+    bankAccounts.forEach((account) =>
+      setAccountToBalance(
+        Object.assign(accountToBalance, {
+          [account.name]: account.balances.current,
         })
+      )
+    );
 
-        // take the sum
-        let balance = Object.values(accountToBalance).reduce((a, b) => a + b, 0)
-        console.log(3)
-        let tempBalanceTrend = []
-        console.log(tempBalanceTrend)
+    // Get the balance by trend
+    const t = await api.getAllTransactions();
+    setTransactions(t);
 
-        Object.entries(dateToNet).forEach(entry => {
-            let [date, net] = entry
-            balance += net
+    const dateToNet = {};
+    t.forEach((transaction) => {
+      const date = transaction.date;
+      const amount = transaction.amount;
+      const val = dateToNet[date] ? dateToNet[date] + amount : amount;
+      dateToNet[date] = val;
+    });
 
-            // assume that dateToNet will be iterated in reverse chronological order
-            tempBalanceTrend = [[date, balance], ...tempBalanceTrend]
+    // take the sum
+    let balance = Object.values(accountToBalance).reduce((a, b) => a + b, 0);
+    console.log(3);
+    let tempBalanceTrend = [];
+    console.log(tempBalanceTrend);
 
-        })
+    Object.entries(dateToNet).forEach((entry) => {
+      let [date, net] = entry;
+      balance += net;
 
-        setBalanceTrend([["Date", "Balance"], ...tempBalanceTrend])
+      // assume that dateToNet will be iterated in reverse chronological order
+      tempBalanceTrend = [[date, balance], ...tempBalanceTrend];
+    });
 
+    setBalanceTrend([["Date", "Balance"], ...tempBalanceTrend]);
 
-        const categoryToMoneySpent = {}
-        const dateToMoneySpent = {}
+    const categoryToMoneySpent = {};
+    const dateToMoneySpent = {};
 
-        // Spending stuff
-        t.forEach(transaction => {
-            const category = transaction.category;
-            const date = transaction.date;
-            const moneySpent = Math.max(0, transaction.amount);
+    // Spending stuff
+    t.forEach((transaction) => {
+      const category = transaction.category;
+      const date = transaction.date;
+      const moneySpent = Math.max(0, transaction.amount);
 
-            if (categoryToMoneySpent[category]) {
-                categoryToMoneySpent[category] += moneySpent;
-            } else {
-                categoryToMoneySpent[category] = moneySpent;
-            }
+      if (categoryToMoneySpent[category]) {
+        categoryToMoneySpent[category] += moneySpent;
+      } else {
+        categoryToMoneySpent[category] = moneySpent;
+      }
 
-            if (dateToMoneySpent[date]) {
-                dateToMoneySpent[date] += moneySpent;
-            } else {
-                dateToMoneySpent[date] = moneySpent;
-            }
-        });
+      if (dateToMoneySpent[date]) {
+        dateToMoneySpent[date] += moneySpent;
+      } else {
+        dateToMoneySpent[date] = moneySpent;
+      }
+    });
 
-        let tempSpendingByCategories = []
-        let tempSpendingTrend = []
-        Object.entries(categoryToMoneySpent).forEach(entry => tempSpendingByCategories = [entry, ...tempSpendingByCategories])
-        setSpendingByCategories([["Category", "Money Spent"], ...tempSpendingByCategories])
+    let tempSpendingByCategories = [];
+    let tempSpendingTrend = [];
+    Object.entries(categoryToMoneySpent).forEach(
+      (entry) =>
+        (tempSpendingByCategories = [entry, ...tempSpendingByCategories])
+    );
+    setSpendingByCategories([
+      ["Category", "Money Spent"],
+      ...tempSpendingByCategories,
+    ]);
 
-        Object.entries(dateToMoneySpent).forEach(entry => tempSpendingTrend = [entry, ...tempSpendingTrend])
-        setSpendingTrend([["Date", "Money Spent"], ...tempSpendingTrend])
-    }, []);
+    Object.entries(dateToMoneySpent).forEach(
+      (entry) => (tempSpendingTrend = [entry, ...tempSpendingTrend])
+    );
+    setSpendingTrend([["Date", "Money Spent"], ...tempSpendingTrend]);
+  }, []);
 
-    const stats = {
-        accountToBalance: accountToBalance,
-        balanceTrend: balanceTrend,
-        spendingByCategories: spendingByCategories,
-        spendingTrend: spendingTrend
-    }
+  const stats = {
+    accountToBalance: accountToBalance,
+    balanceTrend: balanceTrend,
+    spendingByCategories: spendingByCategories,
+    spendingTrend: spendingTrend,
+  };
 
   return (
     <div>
@@ -136,7 +143,7 @@ const Homepage = () => {
           variant="contained"
           id="new-account-btn"
           component={Link}
-          to="/statistics"
+          to="/statistics/spending"
         >
           {" "}
           See More
