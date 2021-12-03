ec24ca338e11d347869be2170cdafd49dd89017f
Jennifer Zeng <zengzhuoranjennifer@gmail.com>
2021-12-03 16:35:40 -0500

    fix deployment server unresponsive to backend code changes

--- back-end/server.js
+++ back-end/server.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 const appModule = require("./app"); // load up the web server
 const server = appModule.app;
-const port = 5000; // the port to listen to for incoming requests
+const port = 8000; // the port to listen to for incoming requests
 // call express's listen function to start listening to the port
 const listener = server.listen(port, function () {
   console.log(`Server running on port: ${port}`);
