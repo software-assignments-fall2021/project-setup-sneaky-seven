16ce2d945d0f7b82c7b286d9748db4c091fe1cf5
tangym27 <mt4119@nyu.edu>
2021-12-09 16:25:50 -0500

    added labels

--- front-end/src/components/CategoryIcon.js
+++ front-end/src/components/CategoryIcon.js
@@ -7,21 +7,37 @@ export default function CategoryIcon({
   size = 64,
   color = "black",
   borderColor = "black",
+  name,
 }) {
   const Icon = iconNameToComponent[icon] ?? FaMoneyBillWave;
   return (
-    <div
-      style={{
-        borderRadius: "50%",
-        width: `${size}px`,
-        height: `${size}px`,
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        border: "4px solid " + borderColor,
-      }}
-    >
-      <Icon color={color} size={~~(size * 0.8)} />
+    <div>
+      <div
+        style={{
+          borderRadius: "50%",
+          width: `${size}px`,
+          height: `${size}px`,
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          border: "4px solid " + borderColor,
+        }}
+      >
+        <Icon color={color} size={~~(size * 0.8)} />
+      </div>
+      <p
+        style={{
+          width: `${size}px`,
+          fontSize: `15px`,
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          wordBreak: "break-all",
+        }}
+      >
+        {" "}
+        {name}{" "}
+      </p>
     </div>
   );
 }
